88bc46e2899bdb5992d9d4462105068f69702bb0
AbderrahimRezki <abderrahim4cem@gmail.com>
2024-07-09 00:43:39 +0100

    added distance matrix impelementation

--- spatial/distances.py
+++ spatial/distances.py
@@ -1,5 +1,4 @@
-from base_model import Model
-from scipy.spatial import distance_matrix
+from scipy.spatial import distance_matrix as dm
 from unittest import TestCase
 import numpy as np
 
@@ -7,6 +6,11 @@ def distance_matrix(A, B):
     assert isinstance(A, np.ndarray) and isinstance(B, np.ndarray), "Matrices should be numpy arrays"
     assert A.shape[1] == B.shape[1], "Matrices's row vectors should have same dimension."
 
+    AA = (A * A).sum(axis=1).reshape(-1, 1)
+    BB = (B * B).sum(axis=1)
+    AB = A @ B.T
+
+    return np.sqrt(AA + BB - 2 * AB)
 
 
 if __name__ == "__main__":
@@ -16,6 +20,9 @@ if __name__ == "__main__":
         [-1, 0, 1]]
     )
 
-    knn = KNearestNeighbors()
-    knn.fit(X, np.zeros(X.shape[0]))
-    knn.predict([[1, 2, 3]])
+    target = np.array(
+        [[1, 2, 3], 
+        [1, 0, 1]]
+    )
+
+    assert np.all(dm(X, target) == distance_matrix(X, target)), "Test failed."
